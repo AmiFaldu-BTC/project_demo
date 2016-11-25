c6cac7679dfdabd344d0097e29c6db102813c9f1
gaurav <gauravm@thinkbiz.co.in>
2016-11-25 15:06:23 +0530

    fgd

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -38,7 +38,7 @@ class StudentsController < ApplicationController
 
   # PATCH/PUT /students/1
   # PATCH/PUT /students/1.json
-  def update
+  def update2
     respond_to do |format|
       if @student.update(student_params)
         format.html { redirect_to @student, notice: 'Student was successfully updated.' }
